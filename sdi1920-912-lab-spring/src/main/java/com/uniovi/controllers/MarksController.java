ad022ca35a0e0fae7417b799c67187aa09e8ebd4
adnanmouloud <adnanmouloud@gmail.com>
2020-02-15 21:25:13 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi1920-912-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi1920-912-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -10,12 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
 import com.uniovi.services.MarksService;
+import com.uniovi.services.UsersService;
 
 @Controller
 public class MarksController {
 
 	@Autowired // Inyectar el servicio
 	private MarksService marksService;
+	
+	@Autowired
+	private UsersService usersService;
+
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
@@ -43,28 +48,30 @@ public class MarksController {
 		return "redirect:/mark/list";
 	}
 
-//	@RequestMapping("/mark/details")
-//	public String getDetail(@RequestParam Long id) {
-//		return "Getting Detail: " + id;
-//	}
 
 	@RequestMapping(value = "/mark/add")
-	public String getMark() {
+	public String getMark(Model model) {
+		model.addAttribute("usersList", usersService.getUsers());
 		return "mark/add";
 	}
 
 	@RequestMapping(value = "/mark/edit/{id}")
 	public String getEdit(Model model, @PathVariable Long id) {
 		model.addAttribute("mark", marksService.getMark(id));
+		model.addAttribute("usersList", usersService.getUsers());
 		return "mark/edit";
 	}
 
 	@RequestMapping(value = "/mark/edit/{id}", method = RequestMethod.POST)
 	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Mark mark) {
-		mark.setId(id);
-		marksService.addMark(mark);
-		return "redirect:/mark/list";
-		//"redirect:/mark/details/" + id;
+		Mark original = marksService.getMark(id);
+		// modificar solo score y description
+		original.setScore(mark.getScore());
+		original.setDescription(mark.getDescription());
+		marksService.addMark(original);
+
+		return "redirect:/mark/details/"+id;
+		
 	}
 	
 	@RequestMapping("/mark/list/update")
--- sdi1920-912-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1920-912-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -10,12 +10,23 @@ public class Mark {
 	private String description;
 	private Double score;
 	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
 	
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+	
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 	
 	public Mark(){
@@ -45,6 +56,16 @@ public class Mark {
 	public void setScore(Double score) {
 	this.score = score;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 
 }
 
