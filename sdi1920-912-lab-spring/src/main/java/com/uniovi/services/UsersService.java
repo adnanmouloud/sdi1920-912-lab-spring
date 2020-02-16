c6dafd9a9c54c3f2ab01a9d8b02238a56116d4e5
adnanmouloud <adnanmouloud@gmail.com>
2020-02-16 13:05:01 +0100

    SDI-SpringBoot- 5 - Autenticación y control de acceso

--- sdi1920-912-lab-spring/src/main/java/com/uniovi/services/UsersService.java
+++ sdi1920-912-lab-spring/src/main/java/com/uniovi/services/UsersService.java
@@ -3,9 +3,8 @@ package com.uniovi.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.User;
@@ -17,7 +16,8 @@ public class UsersService {
 	@Autowired
 	private UsersRepository usersRepository;
 
-	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<User>();
@@ -30,8 +30,13 @@ public class UsersService {
 	}
 
 	public void addUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		usersRepository.save(user);
 	}
+	
+	public User getUserByDni(String dni) {
+		return usersRepository.findByDni(dni);
+		}
 
 	public void deleteUser(Long id) {
 		usersRepository.deleteById(id);
