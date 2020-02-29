72d345933ab38577155f5be52c0d88dac4e73b1b
adnanmouloud <adnanmouloud@gmail.com>
2020-02-29 17:20:51 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- sdi1920-912-lab-spring/src/test/java/com/uniovi/Sdi1920912LabSpringApplicationTests.java
+++ sdi1920-912-lab-spring/src/test/java/com/uniovi/Sdi1920912LabSpringApplicationTests.java
@@ -1,16 +1,13 @@
 package com.uniovi;
 
-import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.Test;
-import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
-//Ordenamos las pruebas por el nombre del método
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 @SpringBootTest
 class Sdi1920912LabSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+	
+	
+
 
 }
--- sdi1920-912-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi1920-912-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,15 +1,20 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
+//Ordenamos las pruebas por el nombre del método
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
@@ -55,8 +60,32 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
 }
