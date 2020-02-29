43f8abdd7b305b11ab973cf77aeb4ec28b1a542b
adnanmouloud <adnanmouloud@gmail.com>
2020-02-29 13:29:06 +0100

    SDI-Selenium-5.1-ClasePrincipal

--- sdi1920-912-lab-spring/src/test/java/com/uniovi/Sdi1920912LabSpringApplicationTests.java
+++ sdi1920-912-lab-spring/src/test/java/com/uniovi/Sdi1920912LabSpringApplicationTests.java
@@ -1,8 +1,11 @@
 package com.uniovi;
 
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Ordenamos las pruebas por el nombre del método
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 class Sdi1920912LabSpringApplicationTests {
 
